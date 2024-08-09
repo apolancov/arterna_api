61741d1119b8711d5380cf3a0d643732e41420a4
Aris Valdez <samus45@hotmail.es>
2024-08-09 13:05:52 -0400

    add route

--- index.ts
+++ index.ts
@@ -3,8 +3,58 @@ import express, { Request, Response } from "express";
 const app = express();
 const port = 3000;
 
-app.get('/',(request: Request, response: Response)=>{
+type Product = {
+    id: number,
+    description: string,
+    price: number,
+    stock: number
+}
 
-})
+const products: Product[] = [
+    {
+        id: 1,
+        description: "Papa",
+        price: 20,
+        stock: 1000
+    },
+    {
+        id: 2,
+        description: "Pan",
+        price: 10,
+        stock: 1000
+    },
+    {
+        id: 3,
+        description: "Agua",
+        price: 20,
+        stock: 1000
+    }
+];
+
+// endPoint
+// get obtener todos los productos
+app.get('/api/inventory', (request: Request, response: Response) => {
+
+    response.json({
+            data: products
+        });
+});
+
+// post 
+// crear nuevo producto
+app.post('/api/inventory', (request: Request, response: Response) => {
+
+    response.json("Post");
+});
+
+app.put('/api/inventory', (request: Request, response: Response) => {
+
+    response.json("Put");
+});
+
+app.delete('/api/inventory', (request: Request, response: Response) => {
+
+    response.json("Delete");
+});
 
 app.listen(port, () => console.log(`This server is running at port ${port}`));
