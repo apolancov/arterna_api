0a8f8dee92eb97a09745e92659c028473bb51b9e
Aris Valdez <samus45@hotmail.es>
2024-08-12 10:29:41 -0400

    add new routes and validation

--- index.ts
+++ index.ts
@@ -28,7 +28,13 @@ const products: Product[] = [
         description: "Agua",
         price: 20,
         stock: 1000
-    }
+    },
+    {
+        id: 4,
+        description: "Papa",
+        price: 200,
+        stock: 1000
+    },
 ];
 
 // endPoint
@@ -36,9 +42,73 @@ const products: Product[] = [
 app.get('/api/inventory', (request: Request, response: Response) => {
 
     response.json({
-            data: products
-        });
+        data: products
+    });
 });
+
+app.get('/api/inventory/byDescription/:description', (request: Request, response: Response) => {
+    const description = request.params.description;
+
+
+    const result = products.find(
+        (item) => item.description.toLocaleLowerCase() === description.toLocaleLowerCase());
+
+    if (result === undefined) {
+        return response.status(404)
+            .json({
+                msg: "No se encontro producto con la descripcion " + description
+            });
+    }
+    
+    response.json({
+        data: result
+    });
+});
+
+app.get('/api/inventory/filter/:description', (request: Request, response: Response) => {
+    const description = request.params.description;
+
+    const result = products.filter(
+        (item) => item.description.toLocaleLowerCase() === description.toLocaleLowerCase());
+
+    response.json({
+        data: result
+    });
+});
+
+app.get('/api/inventory/:id', (request: Request, response: Response) => {
+    const id = request.params.id;
+
+    const result = products.find(
+        (item) => item.id === Number.parseInt(id));
+
+    response.json({
+        data: result
+    });
+});
+
+app.get('/api/inventory/:description', (request: Request, response: Response) => {
+    const id = request.params.id;
+
+    const result = products.find(
+        (item) => item.id === Number.parseInt(id));
+
+    response.json({
+        data: result
+    });
+});
+
+
+/*
+
+recibir parametros por la ruta 
+localhost:3000/api/inventory/papa
+
+comos hacer un post express
+- body
+- json
+
+*/
 
 // post 
 // crear nuevo producto
